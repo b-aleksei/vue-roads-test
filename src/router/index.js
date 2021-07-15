a2d04bd869c49c542e8882ab0f8297877d3694c4
Aleksei Bugaev <103301@mail.ru>
2021-07-15 20:51:50 +0300

    убирает TestLayout3

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import HalfExample from '@/pages/HalfExample.vue';
+import Index from '@/pages/Index.vue';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import generatedRoutes from 'vue-auto-routing';
+// import generatedRoutes from 'vue-auto-routing';
 
 Vue.use(VueRouter);
 
-/*
-const routes = generatedRoutes.map((route) => {
+/*const routes = generatedRoutes.map((route) => {
   route.path = route.path.replace(/Index/, '');
   return route;
-});
-*/
+});*/
 
 const routes = [
-  {}
-]
+  { path: '/halfexample/:question', component: HalfExample },
+  { path: '/', component: Index },
+];
+
 
 export default new VueRouter({
   mode: 'history',
